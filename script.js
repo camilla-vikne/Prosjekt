7036a4469986e9b6b4fe8f4b17f3921264281b75
kristianb09 <kristianb09.kodehode@gmail.com>
2023-12-01 12:15:46 +0100

    Gjorde ferdig timing function i javascript

--- script.js
+++ script.js
@@ -2,20 +2,33 @@
 
 const buttons = document.querySelectorAll("[data-carousel-button]");
 
+function carousel(button) {
+  const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+  const slides = button
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]");
+
+  const activeSlide = slides.querySelector("[data-active]");
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}
+
+function autoRotate() {
+  const nextBtn = document.querySelector("[data-carousel-button='next']");
+  carousel(nextBtn);
+}
+
+let intervalId = setInterval(autoRotate, 5000);
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]");
-
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
+    clearInterval(intervalId);
+    intervalId = setInterval(autoRotate, 5000);
+    carousel(button);
   });
 });
 
