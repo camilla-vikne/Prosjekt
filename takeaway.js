ea02c35f1306f116a7bb210bdf962977efcadc5e
camillab09 <camillab09.kodehode@gmail.com>
2023-12-02 20:35:00 +0100

    Added form validate in js for takeaway form

---
+++ takeaway.js
@@ -0,0 +1,18 @@
+function validateForm() {
+    let x = document.forms["booking"]["name"].value;
+    if (x === "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    let y = document.forms["booking"]["email"].value;
+    if (y === "") {
+      alert("Email must be filled out");
+      return false;
+    }
+    let z = document.forms["booking"]["phone"].value;
+    if (z === "") {
+      alert("Phone number must be filled out");
+      return false;
+    }
+  }
+  
