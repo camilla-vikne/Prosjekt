732c1b119e818c3383df55e19f8a9f82f6356589
camilla.b09 <camillab09.kodehode@gmail.com>
2023-12-04 11:28:03 +0100

    finished JS for menu, need sme styling for fonts, but main code is ready

--- menu.js
+++ menu.js
@@ -1,84 +1,90 @@
-const menuItems = [
+const menu = [
   {
-    name: "Sakura Maki",
-    price: "89kr",
-    description: "A classic maki, perfect for the sushi novice. ",
-    //ingredients: ["Salmon", "avocado"],
+    id: 1,
+    title: "Sakura Maki",
+    price: 89,
+    desc: "A classic maki, perfect for the sushi novice. ",
+    category: "Maki",
   },
   {
-    name: "Tempura Maki",
-    price: "109kr",
-    description:
-      "A staple on any sushi menu, but with our own signature Oishii orange sauce.",
-    //ingredients: ["Fried scampi", "avocado", "cucumber"],
+    id: 2,
+    title: "Tempura Maki",
+    price: 109,
+    desc: "A staple on any sushi menu, but with our own signature Oishii orange sauce.",
+    category: "Maki",
   },
   {
-    name: "Rainbow Maki",
-    price: "129kr",
-    description:
-      "The classic salmon and cucumber roll mixed with caramelized shiitake and tasty tuna.",
-    //ingredients: ["Salmon", "cucumber", "shiitake mushroom", "tuna"],
+    id: 3,
+    title: "Rainbow Maki",
+    price: 129,
+    desc: "The classic salmon and cucumber roll mixed with caramelized shiitake and tasty tuna.",
+    category: "Maki",
   },
   {
-    name: "California Maki",
-    price: "99kr",
-    description: "A true classic signature maki topped with tobiko roe.",
-    //ingredients: ["Imitation crab", "cucumber", "cream cheese"],
+    id: 4,
+    title: "California Maki",
+    price: 99,
+    desc: "A true classic signature maki topped with tobiko roe.",
+    category: "Maki",
   },
   {
-    name: "Haru Maki",
-    price: "169kr",
-    description:
-      "A tasty roll with salmon and avocado, topped with sweet honey and mango sauce.",
-    //ingredients: ["Salmon", "cream cheese", "cucumber", "ikura"],
+    id: 5,
+    title: "Haru Maki",
+    price: 169,
+    desc: "A tasty roll with salmon and avocado, topped with sweet honey and mango sauce.",
+    category: "Maki",
   },
   {
-    name: "Hotate Maki",
-    price: "189kr",
-    description:
-      "Classic roll with a twist. We've added fresh tobiko roes and coriander to bring this roll to a new level. ",
-    //ingredients: ["Scallops", "cucumber", "avocado", "yuzu kosho sauce"],
+    id: 6,
+    title: "Hotate Maki",
+    price: 189,
+    desc: "Classic roll with a twist. We've added fresh tobiko roes and coriander to bring this roll to a new level. ",
+    category: "Maki",
   },
   {
-    name: "Ebi Ringo",
-    price: "179kr",
-    description:
-      "A tasty surprise of fried scampi with sweet apple and crunchy peanuts. Topped with teriyaki sauce. ",
-    //ingredients: ["Tempura scampi", "cream cheese", "apple", "chili mayo"],
+    id: 7,
+    title: "Ebi Ringo",
+    price: 179,
+    desc: "A tasty surprise of fried scampi with sweet apple and crunchy peanuts. Topped with teriyaki sauce. ",
+    category: "Maki",
   },
   {
-    name: "Crispy Kojo",
-    price: "189kr",
-    description:
-      "Tasty vegetarian roll with crispy wonton. Served warm, topped with sakura sauce.",
-    //ingredients: ["Avocado", "shiitake", "bell pepper", "cream cheese"],
+    id: 8,
+    title: "Crispy Kojo",
+    price: 189,
+    desc: "Tasty vegetarian roll with crispy wonton. Served warm, topped with sakura sauce.",
+    category: "Maki",
   },
   {
-    name: "Spicy Tuna",
-    price: "129kr",
-    description:
-      "A tasty roll with a chili mayo center, crispy scallions and fresh cucumber.",
-    //ingredients: ["Tuna", "scallion", "cucumber"],
+    id: 9,
+    title: "Spicy Tuna",
+    price: 129,
+    desc: "A tasty roll with a chili mayo center, crispy scallions and fresh cucumber.",
+    category: "Maki",
   },
+
   {
-    name: "Sweet Roll",
-    price: "149kr",
-    description:
-      "Fried scampi and avocado topped with Philadelphia and fresh strawberries.",
-    //ingredients: ["Tempura scampi", "avocado", "cream cheese", "strawberry"],
+    id: 10,
+    title: "Sweet Roll",
+    price: 149,
+    desc: "Fried scampi and avocado topped with Philadelphia and fresh strawberries.",
+    category: "Maki",
   },
 ];
 
-const menuList = document.getElementById("menu-items");
-
-// populate menu items using loop
-menuItems.forEach((item) => {
-  const menuItem = document.createElement("li");
-  menuItem.textContent = `${item.name}- ${item.price}
-  ${item.description}  `;
-  menuList.appendChild(menuItem);
-});
-
-menu.addEventListener("load", () => {
-  menuList.style.display = menuList.style.display === "none" ? "block" : "none";
+const sectionCenter = document.querySelector(".section-center");
+window.addEventListener("DOMContentLoaded", function () {
+  let displayMenu = menu.map(function (item) {
+    return `<article class="menu-item">
+            <div class="heading">
+                <h4 class="titles">${item.title} </h4>
+            </div>
+           <div class="item-info">
+            <p class="item-text">${item.desc}</p>
+            <h4 class="price">${item.price}kr</h4>
+        </div>
+    </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
 });
